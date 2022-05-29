74ba67d16f916286a266b454d653605683f32698
kartik <kartikmakhloga20@gmail.com>
2022-05-29 20:51:02 +0530

    adding files

---
+++ Unit_03/P16_MultiThreading.java
@@ -0,0 +1,16 @@
+public class P16_MultiThreading {
+
+    public static void main(String[] args){
+        ABC2 obj = new ABC2();
+        obj.threadConcept(){}
+        obj.threadSynchronization(){}
+        obj.interThreadCommunication(){}
+        obj.ProducerConsumerProblems(){}
+        obj.Wait_And_Notify(){}
+    }
+}
+class ABC2{
+
+
+}
+}
